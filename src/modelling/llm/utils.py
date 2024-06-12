981f1ccc8b33eb1f6f00914c32b486a84d4a8b58
mithil <mithilsalunkhe@outlook.com>
2024-06-12 20:48:20 +0530

    Fixed attention pooling not being saved

--- src/modelling/llm/utils.py
+++ src/modelling/llm/utils.py
@@ -7,6 +7,7 @@ from transformers import get_linear_schedule_with_warmup, get_cosine_schedule_wi
 import yaml
 import ast
 
+
 def get_color_escape(r, g, b, background=False):
     return f'\033[{"48" if background else "38"};2;{r};{g};{b}m'
 
@@ -21,8 +22,6 @@ def seed_everything(seed=42):
     torch.backends.cudnn.deterministic = True
 
 
-
-
 def get_score(y_true, y_pred):
     y_true = y_true.cpu().detach().numpy()
     y_pred = y_pred.softmax(dim=1).cpu().detach().numpy()
@@ -30,15 +29,17 @@ def get_score(y_true, y_pred):
 
     return score
 
+
 def compute_metrics(eval_pred):
     predictions, labels = eval_pred
 
     print(predictions.shape, labels.shape)
     loss = log_loss(labels, predictions)
     results = {
-        'log_loss':  loss
+        'log_loss': loss
     }
     return results
+
 
 def get_scheduler(scheduler_name, warmup_steps, optimizer, num_train_steps, num_cycles=1):
     if scheduler_name == 'linear':
@@ -54,11 +55,11 @@ def get_scheduler(scheduler_name, warmup_steps, optimizer, num_train_steps, num_
     return scheduler
 
 
-
-
 def write_yaml(config: dict, save_path: str) -> None:
     with open(save_path, 'w') as f:
         yaml.dump(config, f, )
+
+
 def find_all_linear_names(model):
     cls = torch.nn.Linear
     lora_module_names = set()
@@ -69,10 +70,13 @@ def find_all_linear_names(model):
 
     if 'linear_head' in lora_module_names:  # needed for 16-bit
         lora_module_names.remove('linear_head')
+    if "attention_weights" in lora_module_names:
+        lora_module_names.remove("attention_weights")
     return list(lora_module_names)
+
+
 def string_to_list(s):
     try:
         return ast.literal_eval(s)
     except ValueError:
         return []  # Handle cases where conversion fails
-
