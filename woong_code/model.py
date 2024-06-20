813246a0ac040d6d15ef41095b8b578565ad9903
statking <60002940+kimwoonggon@users.noreply.github.com>
2024-06-20 13:31:07 +0900

    Add files via upload

---
+++ woong_code/model.py
@@ -0,0 +1,59 @@
+import torch
+from torch import nn
+from transformers import LlamaPreTrainedModel
+import torch.nn.functional as F
+
+
+def mean_pooling(token_embeddings, attention_mask):
+    input_mask_expanded = (
+        attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    )
+    return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(
+        input_mask_expanded.sum(1), min=1e-9
+    )
+
+
+class AttentionPooling(nn.Module):
+    def __init__(self, hidden_dim):
+        super(AttentionPooling, self).__init__()
+        self.attention_weights = nn.Linear(hidden_dim, 1, bias=False)
+
+    def forward(self, x, attention_mask):
+        scores = self.attention_weights(x)
+        scores = scores.squeeze(-1)
+
+        scores = scores.masked_fill(attention_mask == 0, -float('inf'))
+
+        attn_weights = F.softmax(scores, dim=-1)
+
+        attn_weights = attn_weights.unsqueeze(-1)
+        pooled_output = torch.sum(x * attn_weights, dim=1)
+
+        return pooled_output
+
+
+class LLamaClassifier(LlamaPreTrainedModel):
+    def __init__(self, model, **kwargs):
+        super().__init__(config=model.config, **kwargs)
+        self.model = model
+        self.model.lm_head = nn.Identity()
+        self.linear_head = nn.Linear(model.config.hidden_size, 3)
+
+        self.dtype_linear = torch.bfloat16
+
+    @staticmethod
+    def mean_pooling(token_embeddings, attention_mask):
+        input_mask_expanded = (
+            attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+        )
+
+        return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(
+            input_mask_expanded.sum(1), min=1e-9
+        )
+
+    def forward(self, input_ids, attention_mask, **kwargs):
+        outputs = self.model(input_ids=input_ids, attention_mask=attention_mask, return_dict=True)
+        hidden_states = outputs['logits']
+        hidden_states = mean_pooling(hidden_states, attention_mask).type(torch.bfloat16)
+
+        return {"logits": self.linear_head(hidden_states)}
---
+++ woong_code/train_fold2_test.py
@@ -0,0 +1,147 @@
+import gc
+import os
+import warnings
+
+import pandas as pd
+from datasets import Dataset
+from peft import prepare_model_for_kbit_training, LoraConfig, get_peft_model, TaskType
+from transformers import AutoTokenizer, TrainingArguments, AutoModelForCausalLM, \
+    BitsAndBytesConfig, DataCollatorWithPadding, Trainer
+from utils import seed_everything, find_all_linear_names, compute_metrics
+import torch
+from torch.nn import functional as F
+from model import LLamaClassifier
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+fold = 2
+CFG = {
+    'seed': 42,
+    'train_csv': '/mnt/alignment-handbook/LMSYS/train_folds_llama.csv',
+    'model_name': 'meta-llama/Meta-Llama-3-8B-Instruct',
+    'max_len': 3096,
+    'batch_size': 8,
+    'num_classes': 3,
+    'model_dir': '/mnt/alignment-handbook/models/Meta-Llama-3-8B-Instruct-3096-2-epoch-label-smoothing-fold{fold}',
+    'epochs': 2,
+    'lr': 4e-5,
+    'mixed_precision': "bf16",
+}
+
+os.environ['WANDB_PROJECT'] = 'lmsys-winner'
+
+
+class CustomTrainer(Trainer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def compute_loss(self, model, inputs, return_outputs=False):
+        labels = inputs.pop("targets").long()
+        outputs = model(**inputs)
+        logits = outputs.get('logits')
+        loss = F.cross_entropy(logits, labels, label_smoothing=0.10)
+        return (loss, outputs) if return_outputs else loss
+
+
+def tokenize_function(examples, tokenizer, max_length):
+    tokenized_inputs = tokenizer(examples["text"], truncation=True, padding="longest", max_length=max_length)
+    return tokenized_inputs
+
+
+def main(cfg):
+    warnings.filterwarnings("ignore")
+    seed_everything(cfg['seed'])
+
+    gc.enable()
+    df = pd.read_csv(cfg['train_csv'])
+    fold = 0
+    train_df = df[df['fold'] != fold].reset_index(drop=True)
+    tokenizer = AutoTokenizer.from_pretrained(cfg['model_name'], add_eos_token=True, trust_remote_code=True)
+    tokenizer.pad_token = tokenizer.eos_token
+    train_df['len'] = train_df['text'].apply(lambda x: len(tokenizer(x)['input_ids']))
+    train_df = train_df[train_df['len'] < cfg['max_len']].reset_index(drop=True)
+    valid_df = df[df['fold'] == fold].reset_index(drop=True)
+    valid_df['len'] = valid_df['text'].apply(lambda x: len(tokenizer(x)['input_ids']))
+    valid_df = valid_df[valid_df['len'] < cfg['max_len']].reset_index(drop=True)
+    train_dataset = Dataset.from_dict({"text": train_df['text'], "targets": train_df['label']})
+    valid_dataset = Dataset.from_dict({"text": valid_df['text'], "targets": valid_df['label']})
+    tokenizer.padding_side = "right"
+
+    training_args = TrainingArguments(
+        per_device_train_batch_size=cfg['batch_size'],
+        num_train_epochs=cfg['epochs'],
+        bf16=True,
+        output_dir=cfg['model_dir'],
+        gradient_checkpointing=True,
+        gradient_accumulation_steps=2,
+        save_strategy="steps",
+        eval_steps=50,
+        overwrite_output_dir=True,
+        learning_rate=float(cfg['lr']),
+        optim="adamw_8bit",
+        seed=42,
+        logging_steps=1,
+        dataloader_num_workers=24,
+        dataloader_pin_memory=True,
+        lr_scheduler_type="cosine",
+        warmup_ratio=0.1,
+        weight_decay=0.01,
+        save_safetensors=True,
+        run_name=cfg['model_dir'].split("/")[-1],
+        remove_unused_columns=False,
+        eval_strategy="steps",
+        metric_for_best_model="log_loss",
+        label_names=["targets"],
+
+    )
+
+    quant_config = BitsAndBytesConfig(
+        load_in_8bit=True,
+    )
+
+    model = AutoModelForCausalLM.from_pretrained(cfg['model_name'], trust_remote_code=True,
+                                                 attn_implementation="flash_attention_2",
+                                                 torch_dtype=torch.float16, quantization_config=quant_config)
+    model = prepare_model_for_kbit_training(model, use_gradient_checkpointing=True)
+    model.gradient_checkpointing_enable()
+    model.config.use_cache = False
+    model = LLamaClassifier(model, torch_dtype=torch.bfloat16)
+    print("Linear layers: ", find_all_linear_names(model))
+    peft_config = LoraConfig(
+        r=64,
+        lora_alpha=128,
+        lora_dropout=0.05,
+        bias="none",
+        target_modules=find_all_linear_names(model),
+        task_type=TaskType.SEQ_CLS,
+        modules_to_save=["linear_head", ],
+    )
+
+    model = get_peft_model(model, peft_config)
+    model.print_trainable_parameters()
+    for name, param in model.linear_head.named_parameters():
+        param.requires_grad = True
+
+    train_dataset = train_dataset.map(lambda x: tokenize_function(x, tokenizer, cfg['max_len']))
+    valid_dataset = valid_dataset.map(lambda x: tokenize_function(x, tokenizer, cfg['max_len']))
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer, )
+    train_dataset = train_dataset.remove_columns(["text"])
+    valid_dataset = valid_dataset.remove_columns(["text"])
+    trainer = CustomTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        tokenizer=tokenizer,
+        data_collator=data_collator,
+        compute_metrics=compute_metrics,
+        eval_dataset=valid_dataset,
+
+    )
+
+    trainer.train()
+    print(model)
+    trainer.save_model(cfg['model_dir'])
+
+
+if __name__ == '__main__':
+    main(CFG)
